c3b8a02a5065acca2664cc8bc66cfb72ae012fd6
Gabriel Howard <j0s3ghoward@gmail.com>
2020-04-13 23:01:45 +0200

    vendor: cafstyle: Add build modes for more chipset support Now you must use TARGET_BOOT_MODE on your tree to define which type to boot mode your chip uses: enforcing, permissive or disabled. This support more chips and devices, not only new (enforcing) based devices Signed-off-by: Gabriel Howard <j0s3ghoward@gmail.com>

---
+++ vendor/cafstyle/boot/permissive/CAF.mk
@@ -0,0 +1,16 @@
+# Copyright 2020 The Code Aurora Forum Project
+# Copyright 2020 The Brain Repo Project
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := product_boot_res_permissive
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := $(_img_modules) $(_anim_modules)
+include $(BUILD_PHONY_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := SLQLib
+LOCAL_MODULE_TAGS := optional
+LOCAL_REQUIRED_MODULES := $(_img_modules) $(_anim_modules)
+include $(BUILD_PHONY_PACKAGE)
---
+++ vendor/cafstyle/config.mk
@@ -0,0 +1,47 @@
+# Copyright (C) 2030 The Brain Repo Project
+# Copyright (C) 2020 The Code Aurora Forum Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Prebuilt Packages
+PRODUCT_PACKAGES += \
+    IntelBootPickerPrebuilt \
+    NexusLauncherPrebuilt \
+    WellbeingPrebuilt \
+    Fonts
+
+ifeq ($(TARGET_APPS_ARCH),arm64)
+PRODUCT_PACKAGES += \
+    MatchmakerPrebuilt
+else ifeq ($(TARGET_APPS_ARCH),x86)
+PRODUCT_PACKAGES += \
+    IntelLegacyPrebuilt
+endif
+
+TARGET_MINIMAL_APPS ?= false
+
+# Boot Mode
+ifeq ($(TARGET_BOOT_MODE),enforcing)
+     PRODUCT_COPY_FILES += vendor/cafstyle/boot/enforcing/CAF.mk
+else ifeq ($(TARGET_BOOT_MODE),permissive)
+     PRODUCT_COPY_FILES += vendor/cafstyle/boot/permissive/CAF.mk
+else ifeq ($(TARGET_BOOT_MODE),disabled)
+     PRODUCT_COPY_FILES += vendor/cafstyle/boot/disabled/CAF.mk
+else
+    ifeq ($(TARGET_BOOT_MODE),)
+        $(error "CAFStyle: TARGET_BOOT_MODE is undefined, you must sepcify the mode on your chipset tree")
+    endif
+endif
+
+# Files
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/etc,$(TARGET_COPY_OUT_PRODUCT)/etc)
