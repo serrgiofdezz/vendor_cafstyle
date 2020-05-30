fd04d2965918693b044f5f8737b97a196b1d5618
Gabriel Howard <j0s3ghoward@gmail.com>
2020-05-30 14:18:57 +0200

    vendor: cafstyle: Remove unnecesary lib
    Should be added on device tree for each device

--- vendor/cafstyle/boot/permissive/CAF.mk
+++ vendor/cafstyle/boot/permissive/CAF.mk
@@ -4,12 +4,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := product_boot_res_permissive
-LOCAL_MODULE_TAGS := optional
-LOCAL_REQUIRED_MODULES := $(_img_modules) $(_anim_modules)
-include $(BUILD_PHONY_PACKAGE)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := SLQLib
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := $(_img_modules) $(_anim_modules)
