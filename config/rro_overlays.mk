1391d146daa9b71da67c2af546845343b42be8c5
Dyneteve <dyneteve@hentaios.com>
2020-12-24 23:04:24 +0900

    cafstyle: fonts: Import Manrope (2/2)
    Signed-off-by: Paloma García <gpaloma.m@hotmail.com>

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -24,6 +24,7 @@ PRODUCT_PACKAGES += \
     FontArbutusSourceOverlay \
     FontArvoLatoOverlay \
     FontKaiOverlay \
+		FontManropeOverlay \
     FontRubikRubikOverlay \
     FontSamOverlay \
     FontVictorOverlay \
