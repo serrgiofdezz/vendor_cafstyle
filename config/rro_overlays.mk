546b9e139413b4fc84d61680ea9d29b5f2482084
Paloma García <gpaloma.m@hotmail.com>
2021-03-07 20:03:25 +0100

    cafstyle: rro: Drop ElementOSWallsStubOverlay
    We're not longer bringing those wallpapers
    
    Signed-off-by: Paloma García <gpaloma.m@hotmail.com>

--- config/rro_overlays.mk
+++ config/rro_overlays.mk
@@ -20,7 +20,6 @@ PRODUCT_PACKAGES += \
     AccentColorHentaiOSOVerlay \
     AccentColorTealOverlay \
     AccentcolorStarbucksOverlay \
-    ElementOSWallsStubOverlay \
     FontArbutusSourceOverlay \
     FontArvoLatoOverlay \
     FontKaiOverlay \
